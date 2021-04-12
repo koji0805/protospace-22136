b10d18379f3b284348da0255cb0a52047a9c96e3
kojio0805 <koji.0805.fuji@gmail.com>
2021-04-12 21:24:44 +0900

    新規登録機能

--- db/migrate/20210302111229_devise_create_users.rb
+++ db/migrate/20210302111229_devise_create_users.rb
@@ -6,10 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name,               null: false,
-      t.string :profile,            null: false,
-      t.string :occupation,         null: false,
-      t.string :position,           null: false,
+      t.string :name,               null: false
+      t.string :profile,            null: false
+      t.string :occupation,         null: false
+      t.string :position,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
