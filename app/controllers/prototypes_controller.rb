11b37c190f771054be0a9d2c7534ced5a86e18d3
kojio0805 <koji.0805.fuji@gmail.com>
2021-04-25 20:11:56 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,27 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  
   def index
+    @prototypes = Prototype.includes(:user)
   end
+
   def new
+    @prototype = Prototype.new
   end
+
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
