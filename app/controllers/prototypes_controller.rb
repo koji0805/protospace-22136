8e345d05402b208f8c1a71e6174e1d0896b24248
kojio0805 <koji.0805.fuji@gmail.com>
2021-04-26 19:12:31 +0900

    画像投稿機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,4 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, except: [:index, :new, :create]
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -23,5 +22,4 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
 end
