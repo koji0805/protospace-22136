97d12c0590a69358ae4bb7c3635aaa6a15bf7d60
kojio0805 <koji.0805.fuji@gmail.com>
2021-05-01 16:34:47 +0900

    詳細ページ修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,10 +22,8 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    # binding.pry
-     @prototype = Prototype.find(params[:id])
      @comment = Comment.new
-     @comments = @prototype.comments.includes(:user)
+     @comments = @prototype.comments
   end
 
   def edit
